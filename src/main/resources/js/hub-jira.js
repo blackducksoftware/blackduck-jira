08113021f97c1bbd1c5ef27bf206cd0177866262
jrichard <jrichard@blackducksoftware.com>
2016-06-23 14:09:12 -0400

    IJRAH-42 Fixing the configuration based on suggestions from Karen

--- src/main/resources/js/hub-jira.js
+++ src/main/resources/js/hub-jira.js
@@ -650,14 +650,14 @@ function removeClassFromField(field, cssClass){
 
 function toggleDisplay(icon, fieldId){
 	var iconObject = AJS.$(icon);
-	if(iconObject.hasClass('fa-minus-circle')){
-		removeClassFromField(icon, 'fa-minus-circle');
-		addClassToField(icon, 'fa-plus-circle');
+	if(iconObject.hasClass('fa-angle-down')){
+		removeClassFromField(icon, 'fa-angle-down');
+		addClassToField(icon, 'fa-angle-right');
 		
 		addClassToFieldById(fieldId, hiddenClass);
-	} else if(iconObject.hasClass('fa-plus-circle')){
-		removeClassFromField(icon, 'fa-plus-circle');
-		addClassToField(icon, 'fa-minus-circle');
+	} else if(iconObject.hasClass('fa-angle-right')){
+		removeClassFromField(icon, 'fa-angle-right');
+		addClassToField(icon, 'fa-angle-down');
 	
 		removeClassFromFieldById(fieldId, hiddenClass);
 	}
@@ -671,7 +671,7 @@ function addMappingErrorStatus(mappingElement){
 		AJS.$(newStatus).addClass("error");
 		AJS.$(newStatus).addClass("largeIcon");
 		AJS.$(newStatus).addClass("fa");
-		AJS.$(newStatus).addClass("fa-times");
+		AJS.$(newStatus).addClass("fa-exclamation");
 
 		newStatus.appendTo(mappingStatus);
 	}
