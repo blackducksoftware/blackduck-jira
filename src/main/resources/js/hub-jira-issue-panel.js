17ba05522bd2b0afd4176a571a488b7c60484e0b
jrichard <jrichard@blackducksoftware.com>
2016-09-12 16:26:28 -0400

    IJRAH-96 adding our own panel and displaying our fields in it and hiding the custom fields

---
+++ src/main/resources/js/hub-jira-issue-panel.js
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (C) 2016 Black Duck Software, Inc.
+ * http://www.blackducksoftware.com/
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *******************************************************************************/
+var customFieldsModuleId = "customfieldmodule";
+
+var hubCustomFields = ["Hub Project", "Hub Project Version", "Hub Component", "Hub Component Version", "Hub Policy Rule"];
+
+AJS.$(function() {
+	hideHubCustomFields();
+	});
+
+function hideHubCustomFields() {
+	var customFieldsModule = AJS.$('#' + customFieldsModuleId);
+	if(customFieldsModule.length > 0){
+		var customFieldPropertyList =  AJS.$(customFieldsModule).find(".property-list");
+		if(customFieldPropertyList.length > 0){
+			var properties = customFieldPropertyList.children();
+			if(properties.length > 0){
+				for(i=0; i < properties.length; i++){
+					checkPropertyAndHideHubField(properties[i]);
+				}
+			}
+		}
+	}
+}
+
+
+function checkPropertyAndHideHubField(property){
+	var customFieldPropertyLabel =  AJS.$(property).find("strong.name");
+	
+	var customFieldName = AJS.$(customFieldPropertyLabel).prop("title");
+	var arrayIndex = hubCustomFields.indexOf(customFieldName);
+	if(arrayIndex >= 0){
+		AJS.$(property).css("display", "none");
+	}
+}
