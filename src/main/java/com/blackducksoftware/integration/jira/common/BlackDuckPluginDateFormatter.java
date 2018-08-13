145e39d461852ccb69f52ac082b8f4476d36dfb4
Gavin Killough <gavink@synopsys.com>
2018-08-13 08:55:43 -0400

    Move date formatter into common package.

--- src/main/java/com/blackducksoftware/integration/jira/common/BlackDuckPluginDateFormatter.java
+++ src/main/java/com/blackducksoftware/integration/jira/common/BlackDuckPluginDateFormatter.java
@@ -21,7 +21,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package com.blackducksoftware.integration.jira;
+package com.blackducksoftware.integration.jira.common;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
