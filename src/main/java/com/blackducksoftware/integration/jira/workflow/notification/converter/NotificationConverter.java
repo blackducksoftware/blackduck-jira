74c82f72189d18b4cff65eaa350ffbbb76acb064
Brian Mandel <bmandel@blackducksoftware.com>
2019-08-01 16:26:12 -0400

    fix(JAVA): Was not correctly returning a value that was expected from the method signature.

--- src/main/java/com/blackducksoftware/integration/jira/workflow/notification/converter/NotificationConverter.java
+++ src/main/java/com/blackducksoftware/integration/jira/workflow/notification/converter/NotificationConverter.java
@@ -1,18 +1,27 @@
 package com.blackducksoftware.integration.jira.workflow.notification.converter;
 
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 import com.blackducksoftware.integration.jira.issue.model.BlackDuckIssueModel;
+import com.blackducksoftware.integration.jira.workflow.notification.NotificationContentDetail;
 import com.blackducksoftware.integration.jira.workflow.notification.NotificationDetailResult;
 import com.synopsys.integration.blackduck.api.generated.enumeration.NotificationType;
 
 public class NotificationConverter {
 
-    public BlackDuckIssueModel convertFromNotificationToModel(NotificationDetailResult notificationDetailResult) {
+    public List<BlackDuckIssueModel> convertFromNotificationToModels(NotificationDetailResult notificationDetailResult) {
         final Map<NotificationType, NotificationTypeParser> parserMapping = createParserMapping();
         final NotificationType type = notificationDetailResult.getType();
-        final BlackDuckIssueModel blackDuckIssueModel = parserMapping.get(type).parseNotification(notificationDetailResult);
+        List<BlackDuckIssueModel> convertedIssues = new LinkedList<>();
+        for (NotificationContentDetail notificationContentDetail : notificationDetailResult.getNotificationContentDetails()) {
+            final BlackDuckIssueModel blackDuckIssueModel = parserMapping.get(type).parseNotification(notificationContentDetail);
+            convertedIssues.add(blackDuckIssueModel);
+        }
+
+        return convertedIssues;
     }
 
     public <N extends NotificationTypeParser> Map<NotificationType, N> createParserMapping() {
--- src/main/java/com/blackducksoftware/integration/jira/workflow/notification/converter/NotificationTypeParser.java
+++ src/main/java/com/blackducksoftware/integration/jira/workflow/notification/converter/NotificationTypeParser.java
@@ -1,9 +1,9 @@
 package com.blackducksoftware.integration.jira.workflow.notification.converter;
 
 import com.blackducksoftware.integration.jira.issue.model.BlackDuckIssueModel;
-import com.blackducksoftware.integration.jira.workflow.notification.NotificationDetailResult;
+import com.blackducksoftware.integration.jira.workflow.notification.NotificationContentDetail;
 
 public abstract class NotificationTypeParser {
 
-    public abstract BlackDuckIssueModel parseNotification(NotificationDetailResult detailResult);
+    public abstract BlackDuckIssueModel parseNotification(NotificationContentDetail notificationContentDetail);
 }
